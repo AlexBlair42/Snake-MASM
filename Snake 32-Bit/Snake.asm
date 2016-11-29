421674f649ed5b22a4fdf21c1a05aa725e0b9f11
Alex Blair <ablair18@my.whitworth.edu>
2016-11-29 13:44:04 -0800

    Organizational Changes

--- Snake 32-Bit/Snake.asm
+++ Snake 32-Bit/Snake.asm
@@ -1,15 +1,17 @@
 TITLE   Snake   (Snake.asm)
+INCLUDE Irvine32.inc
 
+; Last update: 11/29/16
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; This program: 
 ;Simulates the game of Snake. 
 ;The snake will move via user input as well as it shall eat and grow according to food eaten.
 ; Then the game will end if the snake collides with a wall, or if the snake collides with itself.
 ;Also the next level will start if the snake finishes all of its food. 
 ;Lastly the user may end the game by pressing the escape key.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Last update: 11/29/16
-
-INCLUDE Irvine32.inc
 .data
 
 .code
